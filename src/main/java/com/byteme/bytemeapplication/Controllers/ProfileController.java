4afcaca1ce97324c4c2bcf3b01497176ce5e6130
suhleena <161402716+suhleena@users.noreply.github.com>
2025-04-10 10:34:18 +1000

    Add ProfileEdit.png

--- src/main/java/com/byteme/bytemeapplication/Controllers/ProfileController.java
+++ src/main/java/com/byteme/bytemeapplication/Controllers/ProfileController.java
@@ -1,4 +1,17 @@
 package com.byteme.bytemeapplication.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.Pane;
+import java.io.IOException;
+
+
 public class ProfileController {
+
 }
