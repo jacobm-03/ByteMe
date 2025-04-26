03deec4565aa172c08fcf54317034c4e0ceae082
Jack Cumner <jackcumner7@gmail.com>
2025-04-26 13:17:21 +1000

    Added log out function and user dropdown

--- src/main/java/com/byteme/bytemeapplication/Controllers/HomeController.java
+++ src/main/java/com/byteme/bytemeapplication/Controllers/HomeController.java
@@ -2,17 +2,23 @@ package com.byteme.bytemeapplication.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Side;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.net.URL;
 import com.byteme.bytemeapplication.Helpers.Session;
 import com.byteme.bytemeapplication.Models.User;
 
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ContextMenu;
 
 public class HomeController {
 
@@ -40,6 +46,8 @@ public class HomeController {
             } else {
                 userNameLabel.setText("Guest");
             }
+
+            userDrop(); // sets up dropdown when clicking on user
 
             loadContent("/com/byteme/bytemeapplication/fxml/HomeContent.fxml");
         } catch (IOException e) {
@@ -81,6 +89,41 @@ public class HomeController {
             e.printStackTrace();
         }
     }
+
+    public void userDrop(){ //creates the user dropdown
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem logoutItem = new MenuItem(" Log out");
+        logoutItem.setOnAction(e -> hLogout());
+
+        contextMenu.getItems().add(logoutItem);
+
+        userNameLabel.setOnMouseClicked(event -> {
+            if (event.getButton().name().equals("PRIMARY")) {
+                contextMenu.show(userNameLabel, Side.BOTTOM, 0, 0); //Sets dropdown below username
+
+            }
+        });
+    }
+
+    private void hLogout() {
+        System.out.println("Logging out");
+        Session.setCurrentUser(null); // Logs out user
+
+        try {
+            // Redirects user to login page
+            Parent loginRoot = FXMLLoader.load(getClass().getResource("/com/byteme/bytemeapplication/fxml/LoginView.fxml"));
+            Stage stage = (Stage) userNameLabel.getScene().getWindow();
+            Scene loginScene = new Scene(loginRoot);
+
+            stage.setScene(loginScene);
+            stage.sizeToScene(); //resets the page size
+
+        } catch (IOException e) {
+            System.out.println("❌ Failed to log out");
+            e.printStackTrace();
+        }
+    }
+
 
     public void loadContent(String fxmlPath) throws IOException {
         URL url = getClass().getResource(fxmlPath);
