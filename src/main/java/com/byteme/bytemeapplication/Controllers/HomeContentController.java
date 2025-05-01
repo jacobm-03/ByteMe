13005c013738ec25684ef670525b7950d13c3447
jasonbk21 <jasonbenny21@gmail.com>
2025-05-01 20:31:27 +1000

    home to courses button

--- src/main/java/com/byteme/bytemeapplication/Controllers/HomeContentController.java
+++ src/main/java/com/byteme/bytemeapplication/Controllers/HomeContentController.java
@@ -2,8 +2,21 @@ package com.byteme.bytemeapplication.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import java.io.IOException;
+import java.net.URL;
 
 public class HomeContentController {
-
-
+    @FXML
+    private void handleCoursesClick(MouseEvent event) {
+        try {
+            HomeController.getInstance().loadContent("/com/byteme/bytemeapplication/fxml/CourseView.fxml");
+        } catch (IOException e) {
+            System.out.println("❌ Error loading Courses content");
+            e.printStackTrace();
+        }
+    }
 }
